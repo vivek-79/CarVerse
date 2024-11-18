851364e6b183b8e19d34b326952529b65f5b4576
vivek-79 <vivekroy5667@gmail.com>
2024-11-18 08:44:42 +0530

    error-6

--- src/app/(root)/addCar/page.js
+++ src/app/(root)/addCar/page.js
@@ -1,6 +1,6 @@
 
 'use client'
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import { useForm } from 'react-hook-form'
 import '../root.css'
 import { useRouter } from 'next/navigation'
