6589499f8abdd252bbe101d3153548f6cd8e443d
vivek-79 <vivekroy5667@gmail.com>
2024-11-18 10:54:37 +0530

    13

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 
-export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL
+export const BASE_API_URL = process.env.NODE_ENV == "production"?"https://carverse-theta.vercel.app":"http://localhost:3000";
